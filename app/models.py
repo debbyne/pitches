e3b0b3857086809f2e885421e48c38ed6dd0050d
debbyne <kinyadebbyne@gmail.com>
2022-03-06 17:01:50 +0300

    add db instance

---
+++ app/models.py
@@ -0,0 +1,5 @@
+class User:
+    '''
+    Class that defines user of app
+    '''
+    pass
---
+++ config.py
@@ -0,0 +1,2 @@
+class Config:
+     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitches'
